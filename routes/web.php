cc0504643bad5e9404b7c5dad69cced608139046
lvlykatie <tranthanh04122310@nh04122310gmail.22310gmailcom>
2024-09-22 19:37:26 +0700

    admin panel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+
+Route::get('/admin/products', function () {
+    return view('admin.components.products');
+});
+
+Route::get('/admin/orders', function () {
+    return view('admin.components.orders');
+});
+
+
+Route::get('/admin/users', function () {
+    return view('admin.components.users');
+});
+
+
+Route::get('/admin/deals', function () {
+    return view('admin.components.deals');
+});
+
+Route::get('/admin/account', function () {
+    return view('admin.components.account');
+});
