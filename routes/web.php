ed6bf744af7a04fe26cb26f722e15c8e4cc21204
ThuHuynh1438 <thu0938905081@gmail.com>
2024-09-24 00:02:37 +0700

    sua tum lum css

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('signUp_form'); // Thay 'home' bằng tên file .blade.php của bạn
+});
+
