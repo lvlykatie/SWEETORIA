435e4eea0487b891996d0bcd1cf6832d7c4dbd9a
ThuHuynh1438 <thu0938905081@gmail.com>
2024-09-24 14:42:03 +0700

    chinh sua tieu de form

--- resources/views/signUp_form.blade.php
+++ resources/views/signUp_form.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sweetoria Sign In</title>
+    <title>Sweetoria Sign Up</title>
     <link rel="stylesheet" href="{{ asset('public/frontend/client/form/css/styleform.css') }}" />
     <link
       rel="stylesheet"
