6aa968e4334fa17a9368e30976f0b309bee74332
thiennhat731 <thiennhatnguyen66@gmail.com>
2024-10-14 23:05:04 +0700

    lay product ra page product va home

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class PageController extends Controller
 {
     public function homepage()
     {
-        return view('homepage');
+        $products = Product::all();
+        return view('homepage', ['products' => $products]);
     }
 
     public function cart()
